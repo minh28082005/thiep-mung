2b6531e377b0cea033c3bc60975ab6b378bfa155
minh28082005 <148514184+minh28082005@users.noreply.github.com>
2023-10-20 12:41:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Trang';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Nhân ngày phụ nữ Việt Nam chúc Trang xinh gái được ngàn lời yêu' ;
+const contentCard = 'Chúc Trang tỷ tỷ có 1 ngày 20/10 tràn ngập niềm vui và những nụ cười.Chúc Trang gấm vóc lụa là tay tiêu tiền tỷ lấy chồng đại gia   ' ;
                      
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
